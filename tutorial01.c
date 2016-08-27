6ba5a8cc23047ad1931255f6414ad977e16424e2
feixiao <feixiao2020@sina.com>
2016-08-27 11:14:19 +0800

    添加注释

--- tutorial01.c
+++ tutorial01.c
@@ -10,6 +10,8 @@
 //
 // Use the Makefile to build all examples.
 //
+//      or gcc -Wall -ggdb tutorial01.c -D_GNU_SOURCE=1 -D_REENTRANT -I/usr/local/include -I/usr/include/SDL -c -o obj/tutorial01.o
+//
 // Run using
 //
 // tutorial01 myvideofile.mpg
@@ -27,20 +29,20 @@ void SaveFrame(AVFrame *pFrame, int width, int height, int iFrame) {
   FILE *pFile;
   char szFilename[32];
   int  y;
-  
+
   // Open file
   sprintf(szFilename, "frame%d.ppm", iFrame);
-  pFile=fopen(szFilename, "wb");
-  if(pFile==NULL)
+  pFile = fopen(szFilename, "wb");
+  if(pFile == NULL)
     return;
-  
+
   // Write header
   fprintf(pFile, "P6\n%d %d\n255\n", width, height);
-  
+
   // Write pixel data
   for(y=0; y<height; y++)
     fwrite(pFrame->data[0]+y*pFrame->linesize[0], 1, width*3, pFile);
-  
+
   // Close file
   fclose(pFile);
 }
@@ -50,7 +52,7 @@ int main(int argc, char *argv[]) {
   int             i, videoStream;
   AVCodecContext  *pCodecCtx = NULL;
   AVCodec         *pCodec = NULL;
-  AVFrame         *pFrame = NULL; 
+  AVFrame         *pFrame = NULL;
   AVFrame         *pFrameRGB = NULL;
   AVPacket        packet;
   int             frameFinished;
@@ -59,94 +61,94 @@ int main(int argc, char *argv[]) {
 
   AVDictionary    *optionsDict = NULL;
   struct SwsContext      *sws_ctx = NULL;
-  
+
   if(argc < 2) {
     printf("Please provide a movie file\n");
     return -1;
   }
-  // Register all formats and codecs
+
+  // av_register_all 只需要调用一次，他会注册所有可用的文件格式和编解码库，当文件被打开时他们将自动匹配相应的编解码库。
   av_register_all();
-  
-  // Open video file
+
+  // 从传入的第一个参数获得文件路径，这个函数会读取文件头信息，并把信息保存在 pFormatCtx 结构体当中。
+  // 这个函数后面三个参数分别是： 指定文件格式、 缓存大小和格式化选项，当我们设置为 NULL 或 0 时，libavformat 会自动完成这些工作。
   if(avformat_open_input(&pFormatCtx, argv[1], NULL, NULL)!=0)
     return -1; // Couldn't open file
-  
-  // Retrieve stream information
+
+  // 要得到流信息
   if(avformat_find_stream_info(pFormatCtx, NULL)<0)
     return -1; // Couldn't find stream information
-  
-  // Dump information about file onto standard error
+
+  // 这个函数填充了 pFormatCtx->streams 流信息， 可以通过 dump_format 把信息打印出来：
   av_dump_format(pFormatCtx, 0, argv[1], 0);
-  
-  // Find the first video stream
-  videoStream=-1;
+
+  // 获取第一个视频流
+  videoStream = -1;
   for(i=0; i<pFormatCtx->nb_streams; i++)
     if(pFormatCtx->streams[i]->codec->codec_type==AVMEDIA_TYPE_VIDEO) {
-      videoStream=i;
+      videoStream = i;
       break;
     }
-  if(videoStream==-1)
+  if(videoStream == -1)
     return -1; // Didn't find a video stream
-  
-  // Get a pointer to the codec context for the video stream
-  pCodecCtx=pFormatCtx->streams[videoStream]->codec;
-  
-  // Find the decoder for the video stream
-  pCodec=avcodec_find_decoder(pCodecCtx->codec_id);
-  if(pCodec==NULL) {
+
+  // pCodecCtx 包含了这个流在用的编解码的所有信息，但我们仍需要通过他获得特定的解码器然后打开他。
+  pCodecCtx = pFormatCtx->streams[videoStream]->codec;
+
+  // 为视频流获取特定的解码器。
+  pCodec = avcodec_find_decoder(pCodecCtx->codec_id);
+  if(pCodec == NULL) {
     fprintf(stderr, "Unsupported codec!\n");
     return -1; // Codec not found
   }
   // Open codec
   if(avcodec_open2(pCodecCtx, pCodec, &optionsDict)<0)
     return -1; // Could not open codec
-  
-  // Allocate video frame
-  pFrame=av_frame_alloc();
-  
+
+  //　我们需要内存空间存储一帧数据
+  pFrame = av_frame_alloc();
+
   // Allocate an AVFrame structure
-  pFrameRGB=av_frame_alloc();
+  pFrameRGB = av_frame_alloc();
   if(pFrameRGB==NULL)
     return -1;
-  
-  // Determine required buffer size and allocate buffer
-  numBytes=avpicture_get_size(PIX_FMT_RGB24, pCodecCtx->width,
-			      pCodecCtx->height);
-  buffer=(uint8_t *)av_malloc(numBytes*sizeof(uint8_t));
 
-  sws_ctx =
-    sws_getContext
-    (
-        pCodecCtx->width,
-        pCodecCtx->height,
-        pCodecCtx->pix_fmt,
-        pCodecCtx->width,
-        pCodecCtx->height,
-        PIX_FMT_RGB24,
-        SWS_BILINEAR,
-        NULL,
-        NULL,
-        NULL
-    );
-  
+  // 即使分配了帧空间，我们仍然需要空间来存放转换时的 raw 数据，我们用 avpicture_get_size 来得到需要的空间，然后手动分配。
+  numBytes = avpicture_get_size(PIX_FMT_RGB24, pCodecCtx->width,
+			      pCodecCtx->height);
+  buffer = (uint8_t *)av_malloc(numBytes*sizeof(uint8_t));
+
+  sws_ctx = sws_getContext(
+              pCodecCtx->width,
+              pCodecCtx->height,
+              pCodecCtx->pix_fmt,
+              pCodecCtx->width,
+              pCodecCtx->height,
+              PIX_FMT_RGB24,
+              SWS_BILINEAR,
+              NULL,
+              NULL,
+              NULL
+            );
+
   // Assign appropriate parts of buffer to image planes in pFrameRGB
   // Note that pFrameRGB is an AVFrame, but AVFrame is a superset
   // of AVPicture
+  // 我们使用 avpicture_fill 来关联新分配的缓冲区的帧。 
+  // AVPicture 结构体是 AVFrame 结构体的一个子集，开始的AVFrame 是和 AVPicture 相同的
   avpicture_fill((AVPicture *)pFrameRGB, buffer, PIX_FMT_RGB24,
 		 pCodecCtx->width, pCodecCtx->height);
-  
-  // Read frames and save first five frames to disk
+
+  // 通过包来读取整个视频流， 然后解码到帧当中，一但一帧完成了， 将转换并保存它
   i=0;
   while(av_read_frame(pFormatCtx, &packet)>=0) {
     // Is this a packet from the video stream?
-    if(packet.stream_index==videoStream) {
+    if(packet.stream_index == videoStream) {
       // Decode video frame
-      avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished, 
-			   &packet);
-      
+      avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished, &packet);
       // Did we get a video frame?
       if(frameFinished) {
-	// Convert the image from its native format to RGB
+	    // Convert the image from its native format to RGB
         sws_scale
         (
             sws_ctx,
@@ -157,30 +159,29 @@ int main(int argc, char *argv[]) {
             pFrameRGB->data,
             pFrameRGB->linesize
         );
-	
-	// Save the frame to disk
-	if(++i<=5)
-	  SaveFrame(pFrameRGB, pCodecCtx->width, pCodecCtx->height, 
-		    i);
-      }
+
+	      // Save the frame to disk
+      	if(++i<=5)
+	          SaveFrame(pFrameRGB, pCodecCtx->width, pCodecCtx->height, i);
+        }
     }
-    
+
     // Free the packet that was allocated by av_read_frame
     av_free_packet(&packet);
   }
-  
+
   // Free the RGB image
   av_free(buffer);
   av_free(pFrameRGB);
-  
+
   // Free the YUV frame
   av_free(pFrame);
-  
+
   // Close the codec
   avcodec_close(pCodecCtx);
-  
+
   // Close the video file
   avformat_close_input(&pFormatCtx);
-  
+
   return 0;
 }
